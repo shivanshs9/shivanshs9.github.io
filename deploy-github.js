14e46452fea53ec537937c3b476a608ca04fb844
Shivansh Saini <shivanshs9@gmail.com>
2019-07-21 03:01:37 +0530

    Added npm install for travis

--- deploy-github.js
+++ deploy-github.js
@@ -1,21 +1,24 @@
 const fs = require('fs');
 const ghpages = require('gh-pages');
-const pjson = require('./package.json');
 
-const version = pjson.version;
 const { GITHUB_TOKEN } = process.env;
 const repo = `https://${GITHUB_TOKEN ? `${GITHUB_TOKEN}@` : ''}github.com/shivanshs9/shivanshs9.github.io.git`;
 
-fs.createReadStream('CNAME').pipe(fs.createWriteStream('public/CNAME'));
+const { version } = require('./package.json');
 
-ghpages.publish(
-  'public',
-  {
-    branch: 'master',
-    repo,
-    message: `Commit v${version}`,
-  },
-  () => {
-    console.log('Deploy Complete!');
-  },
-);
+require('child_process').exec('git rev-parse --short HEAD', (_err, stdout) => {
+  const commit = stdout;
+  fs.createReadStream('CNAME').pipe(fs.createWriteStream('public/CNAME'));
+
+  ghpages.publish(
+    'public',
+    {
+      branch: 'master',
+      repo,
+      message: `Commit v${version} - ${commit}`,
+    },
+    () => {
+      console.log('Deploy Complete!');
+    },
+  );
+});
