d028e22765de48c579e45c9e4dd29ce7e13f4ec9
Shivansh Saini <shivanshs9@gmail.com>
2019-07-21 02:55:31 +0530

    Added Travis

--- deploy-github.js
+++ deploy-github.js
@@ -1,12 +1,19 @@
+const fs = require('fs');
 const ghpages = require('gh-pages');
+const pjson = require('./package.json');
 
+const version = pjson.version;
+const { GITHUB_TOKEN } = process.env;
+const repo = `https://${GITHUB_TOKEN ? `${GITHUB_TOKEN}@` : ''}github.com/shivanshs9/shivanshs9.github.io.git`;
 
-// replace with your repo url
+fs.createReadStream('CNAME').pipe(fs.createWriteStream('public/CNAME'));
+
 ghpages.publish(
   'public',
   {
     branch: 'master',
-    repo: 'git@github.com:shivanshs9/shivanshs9.github.io.git',
+    repo,
+    message: `Commit v${version}`,
   },
   () => {
     console.log('Deploy Complete!');
